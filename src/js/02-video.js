c6eef01c5395d1e9c817ed4f46aecff86d6b32b3
Yaroslav Drohoziuk <exmahinator@ukr.net>
2022-08-23 16:36:04 +0300

    Task 02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+const LOCAL_CURRENT_KEY = 'videoplayer-current-time';
+const savedTime = localStorage.getItem(LOCAL_CURRENT_KEY);
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function ({ seconds }) {
+  localStorage.setItem(LOCAL_CURRENT_KEY, seconds);
+};
+
+player.on('timeupdate', onPlay);
+
+if (savedTime !== null) {
+  player.setCurrentTime(Number(savedTime));
+}
