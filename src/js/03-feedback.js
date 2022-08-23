5c07c5b7519167065a67b690ff5700ccdf82f3e9
Yaroslav Drohoziuk <exmahinator@ukr.net>
2022-08-23 17:37:56 +0300

    Add local storage export

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+import localStorage from './storage.js';
 
+// -----------------------------------------------------------------------------
+// const save = (key, value) => {
+//   try {
+//     const serializedState = JSON.stringify(value);
+//     localStorage.setItem(key, serializedState);
+//   } catch (error) {
+//     console.error('Set state error: ', error.message);
+//   }
+// };
+
+// const load = key => {
+//   try {
+//     const serializedState = localStorage.getItem(key);
+//     return serializedState === null ? undefined : JSON.parse(serializedState);
+//   } catch (error) {
+//     console.error('Get state error: ', error.message);
+//   }
+// };
+// -----------------------------------------------------------------------------
+
+const CURRENT_LOCAL_KEY = 'feedback-form-state';
+const formRef = document.querySelector('.feedback-form');
+const userData = {};
+
+formRef.addEventListener('input', throttle(onInput, 500));
+
+function onInput(event) {
+  const { name, value } = event.target;
+  userData[name] = value;
+}
